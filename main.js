fec6799ec934192f43894c78f09932bb26709045
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-25 01:46:20 -0700

    set default to la

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function initialize() {
+  // Make into array of locations for ice cream places
+  var location = {
+    x: 34.069378,
+    y: -118.443281
+    /*
+    sweet rose creamery
+    x: 34.076360,
+    y: -118.354860*/
+  }
   var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    center:new google.maps.LatLng(location.x,location.y),
+    zoom:14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    streetViewControl: false, //no pegman for street view
+    scrollWheel: false
   };
   var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
