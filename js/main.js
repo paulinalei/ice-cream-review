8634dfb74dc1525288a81b2e193f8b764cee243d
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-25 20:47:39 -0700

    click on marker to show infowindow

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function initialize() {
   // Make into array of locations for ice cream places
   var locations = [
     {x: 34.069378, y: -118.443281, name: "UCLA"},
-    {x: 34.076360, y: -118.354860} //sweet rose
+    {x: 34.076360, y: -118.354860, name: "Sweet Rose Creamery"} //sweet rose
     ];
 
   var mapProp = {
@@ -19,9 +19,15 @@ function initialize() {
     var marker = new google.maps.Marker({
       position: {lat: locations[index].x, lng: locations[index].y},
       map: map
-    })
-  };
+    });
 
+    var infowindow = new google.maps.InfoWindow({
+      content: locations[index].name
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    })
+  }
   addMarker(0);
   addMarker(1);
 }
