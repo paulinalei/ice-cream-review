1f73fd0f0cc008cdb63eb4e50e350359685443f5
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-25 18:57:41 -0700

    added marker

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 function initialize() {
   // Make into array of locations for ice cream places
   var location = {
-    x: 34.069378,
-    y: -118.443281
+    lat: 34.069378,
+    lng: -118.443281
     /*
     sweet rose creamery
     x: 34.076360,
     y: -118.354860*/
   }
   var mapProp = {
-    center:new google.maps.LatLng(location.x,location.y),
+    center: location,
     zoom:14,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
     streetViewControl: false, //no pegman for street view
     scrollWheel: false
   };
-  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'wassup'
+  });
+
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
