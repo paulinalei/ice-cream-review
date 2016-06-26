a4289350fc80cc982e23e09f37f468dcbca83bc8
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-25 21:26:31 -0700

    got it so that reviews will display on the side in an array

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 function initialize() {
-  // Make into array of locations for ice cream places
+  // Dictionary of locations and names for ice cream places
   var locations = [
     {x: 34.069378, y: -118.443281, name: "UCLA"},
     {x: 34.076360, y: -118.354860, name: "Sweet Rose Creamery"} //sweet rose
     ];
 
+  // Setting up the map
   var mapProp = {
     center: {lat: locations[0].x, lng: locations[0].y},
     zoom:10,
@@ -13,7 +14,14 @@ function initialize() {
     scrollWheel: false
   };
 
+  // Getting element in html to display the map
   var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+
+  /*
+  * Function to add markers
+  * index corresponds to the index in the locations dictionary
+  * infowindow displays name of the store when marker is clicked on
+  */
 
   function addMarker(index) {
     var marker = new google.maps.Marker({
@@ -28,8 +36,21 @@ function initialize() {
       infowindow.open(map, marker);
     })
   }
+
+  // Adding markers -> could turn this into a for loop if you wanted to
+  // use locations.length for for loop
   addMarker(0);
   addMarker(1);
 }
 
+// Loading the map
 google.maps.event.addDomListener(window, 'load', initialize);
+
+// Object to load personal reviews on the side
+var iceCream = {
+    reviewIceCream: function() {
+      var personalReviews = ["it was lit fam", "lit af"];
+      var writing = document.getElementById("reviews");
+      writing.innerHTML = personalReviews[1];
+  }
+}
