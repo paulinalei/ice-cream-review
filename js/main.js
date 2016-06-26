dad59bec9f20e06a7f88c58b6a19f2b57074eaec
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-25 20:17:00 -0700

    made a function to add markers B)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 function initialize() {
   // Make into array of locations for ice cream places
-  var location = {
-    lat: 34.069378,
-    lng: -118.443281
-    /*
-    sweet rose creamery
-    x: 34.076360,
-    y: -118.354860*/
-  }
+  var locations = [
+    {x: 34.069378, y: -118.443281, name: "UCLA"},
+    {x: 34.076360, y: -118.354860} //sweet rose
+    ];
+
   var mapProp = {
-    center: location,
-    zoom:14,
+    center: {lat: locations[0].x, lng: locations[0].y},
+    zoom:10,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
     streetViewControl: false, //no pegman for street view
     scrollWheel: false
   };
+
   var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
 
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    title: 'wassup'
-  });
+  function addMarker(index) {
+    var marker = new google.maps.Marker({
+      position: {lat: locations[index].x, lng: locations[index].y},
+      map: map
+    })
+  };
 
-
+  addMarker(0);
+  addMarker(1);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
